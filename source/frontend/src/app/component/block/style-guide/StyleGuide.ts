544500b2a0dfdc6be5f892b4e2f7c30e7222c409
wangxb <wangxb@truekey.mobi>
2021-03-19 19:06:07 +0800

    组件 video-overlay开发

--- source/frontend/src/app/component/block/style-guide/StyleGuide.ts
+++ source/frontend/src/app/component/block/style-guide/StyleGuide.ts
@@ -1,11 +1,22 @@
 import AbstractBlock from '../AbstractBlock';
 
 export default class StyleGuide extends AbstractBlock {
-  public static readonly displayName:string = 'style-guide';
+  public static readonly displayName: string = 'style-guide';
+  private readonly videoOverlay = this.getElement<HTMLElement>('.js-video-overlay');
+  private readonly videoOverlayPopup = document.body.querySelector<HTMLElement>(
+    '[data-component=video-overlay]',
+  );
 
-  constructor(el:HTMLElement) {
+  constructor(el: HTMLElement) {
     super(el);
+    if (this.videoOverlay) {
+      this.videoOverlay.addEventListener('click', this.handleVideoOverlay);
+    }
   }
+
+  private handleVideoOverlay = (event: Event): void => {
+    this.videoOverlayPopup.style.display = 'block';
+  };
 
   public dispose() {
     super.dispose();

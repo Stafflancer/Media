87ccacc9201f3be7d7aff060b62b7ff2b2350006
wangxb <wangxb@truekey.mobi>
2021-03-22 16:44:31 +0800

    c10-stock-timeline-card

--- source/frontend/src/app/component/block/c10-stock-timeline-card/C10StockTimelineCard.ts
+++ source/frontend/src/app/component/block/c10-stock-timeline-card/C10StockTimelineCard.ts
@@ -2,9 +2,18 @@ import AbstractBlock from '../AbstractBlock';
 
 export default class C10StockTimelineCard extends AbstractBlock {
   public static readonly displayName: string = 'c10-stock-timeline-card';
+  private static isActive: string = 'is-active';
+  private readonly dotButton = this.element.querySelectorAll('.c10-stock-timeline-card__dot');
 
   constructor(el: HTMLElement) {
     super(el);
+
+    for (let i = 0; i < this.dotButton.length; i++) {
+      this.dotButton[i].addEventListener('click', event => {
+        this.dotButton.forEach(dot => dot.classList.remove(C10StockTimelineCard.isActive));
+        this.dotButton[i].classList.add(C10StockTimelineCard.isActive);
+      });
+    }
   }
 
   public dispose() {

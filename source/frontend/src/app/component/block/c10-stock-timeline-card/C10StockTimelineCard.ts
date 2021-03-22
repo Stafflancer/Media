b9a076bee8adecc917115defa405e9aa238fa0f9
wangxb <wangxb@truekey.mobi>
2021-03-22 19:50:08 +0800

    页面整合

--- source/frontend/src/app/component/block/c10-stock-timeline-card/C10StockTimelineCard.ts
+++ source/frontend/src/app/component/block/c10-stock-timeline-card/C10StockTimelineCard.ts
@@ -6,6 +6,9 @@ export default class C10StockTimelineCard extends AbstractBlock {
   private readonly dotButton = this.element.querySelectorAll('.c10-stock-timeline-card__dot');
   private readonly cardBox = this.element.querySelectorAll('.c10-stock-timeline-card__card');
 
+  // c09-media-box
+  private readonly indicatorBtn = this.getElement<HTMLElement>('.c09-media-box__iconfont');
+
   constructor(el: HTMLElement) {
     super(el);
     // 默认显示第三个位置
@@ -21,7 +24,17 @@ export default class C10StockTimelineCard extends AbstractBlock {
         this.cardBox[i].classList.add(C10StockTimelineCard.isActive);
       });
     }
+
+    // c09-media-box
+    this.indicatorBtn!.addEventListener('click', this.handleIndicatorBtn);
   }
+
+  private handleIndicatorBtn = (event: Event): void => {
+    let windoHeight = document.documentElement.offsetHeight || document.body.offsetHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    document.body.scrollTop = scrollTop + windoHeight;
+    document.documentElement.scrollTop = scrollTop + windoHeight;
+  };
 
   public dispose() {
     super.dispose();

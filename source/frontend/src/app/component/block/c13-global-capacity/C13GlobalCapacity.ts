d8940f7d732879a5337f3eabae309b04c928d904
wangxb <wangxb@truekey.mobi>
2021-03-23 15:00:15 +0800

    c13-global-capacity开发

---
+++ source/frontend/src/app/component/block/c13-global-capacity/C13GlobalCapacity.ts
@@ -0,0 +1,13 @@
+import AbstractBlock from '../AbstractBlock';
+
+export default class C13GlobalCapacity extends AbstractBlock {
+  public static readonly displayName: string = 'c13-global-capacity';
+
+  constructor(el: HTMLElement) {
+    super(el);
+  }
+
+  public dispose() {
+    super.dispose();
+  }
+}

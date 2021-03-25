f25d161d268a3bb5a2389740bee8ecacf693c263
wangxb <wangxb@truekey.mobi>
2021-03-25 13:07:11 +0800

    c21-video-player-section

---
+++ source/frontend/src/app/component/block/c21-video-player-section/C21VideoPlayerSection.stories.ts
@@ -0,0 +1,25 @@
+import { Meta } from '@muban/storybook/dist/client/preview/types-6-0';
+
+export default {
+  title: 'C21VideoPlayerSection',
+  component: require('./c21-video-player-section'),
+  argTypes: {
+    // title: { control: 'text' },
+  },
+  parameters: {
+    source: {
+      data: require('./data/default.yaml'),
+    },
+    docs: {
+      description: {
+        component: 'Component Description',
+      },
+    },
+  },
+} as Meta;
+
+export const Default = () => ({});
+Default.args = require('./data/default.yaml');
+
+// export const Variation = Default.bind({});
+// Variation.args = require('./data/variation.yaml');

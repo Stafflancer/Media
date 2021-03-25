008c898e2082cebcb9f4a6b57b24aaaff82976f8
wangxb <wangxb@truekey.mobi>
2021-03-25 17:16:47 +0800

    yaml数据定义

---
+++ source/frontend/src/app/component/block/c30-calculation/C30Calculation.ts
@@ -0,0 +1,13 @@
+import AbstractBlock from '../AbstractBlock';
+
+export default class C30Calculation extends AbstractBlock {
+  public static readonly displayName: string = 'c30-calculation';
+
+  constructor(el: HTMLElement) {
+    super(el);
+  }
+
+  public dispose() {
+    super.dispose();
+  }
+}

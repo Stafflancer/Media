3ed586dd6483cc973740696be52be31979b8b82e
孙焕子 <8687785+sun-huanzi@user.noreply.gitee.com>
2021-03-26 21:44:38 +0800

    c26 修改

--- source/frontend/src/app/component/block/c26-product-intro/C26ProductIntro.ts
+++ source/frontend/src/app/component/block/c26-product-intro/C26ProductIntro.ts
@@ -6,15 +6,34 @@ export default class C26ProductIntro extends AbstractBlock {
   public static readonly displayName: string = 'c26-product-intro';
 
   private readonly swiperContainer = this.getElement<HTMLElement>('.swiper-container');
+  private readonly playIcon = this.getElement<HTMLElement>('.c26-product-intro__img-icon');
+  private readonly imgView = this.getElement<HTMLElement>('.c26-product-intro__product-img');
 
   constructor(el: HTMLElement) {
     super(el);
 
     let swiper = new Swiper(this.swiperContainer, {
-      mouseWheel: true,
+      slidesPerView: 'auto',
+      mousewheel: true,
+      freeMode: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+      },
     });
+
+    this.playIcon!.addEventListener('click', this.changeImg);
   }
 
+  protected changeImg = (): void => {
+    if (this.playIcon!.classList.contains('active')) {
+      this.playIcon!.classList.remove('active');
+      this.imgView!.setAttribute('src', '../../assets/c26-product-intro/c26-product.png');
+    } else {
+      this.playIcon!.classList.add('active');
+      this.imgView!.setAttribute('src', '../../assets/c26-product-intro/c26-product2.png');
+    }
+  };
   public dispose() {
     super.dispose();
   }
